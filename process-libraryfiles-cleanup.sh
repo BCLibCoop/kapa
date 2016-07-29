453989a307981f810b6f66fc1bdc0bbbae37c128
Jeff Davis <jeff.davis@bc.libraries.coop>
2016-07-29 10:32:13 -0700

    Add ersatz backup rotation to cleanup script.
    Signed-off-by: Jeff Davis <jeff.davis@bc.libraries.coop>

--- process-libraryfiles-cleanup.sh
+++ process-libraryfiles-cleanup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+DATE=`date --rfc-3339=date`
+CUTOFF=`date -d "$DATE-30days" --rfc-3339=date`
 cd /opt/flatfiles
-tar cp * | bzip2 > ../flatfiles-daily/flatfiles.`date --rfc-3339=date`.tar.bz2
+tar cp * | bzip2 > ../flatfiles-daily/flatfiles.${DATE}.tar.bz2
 rm /opt/flatfiles/*/*
+if [ -f "/opt/flatfiles-daily/flatfiles.${CUTOFF}.tar.bz2" ] ; then
+  rm /opt/flatfiles-daily/flatfiles.${CUTOFF}.tar.bz2
+fi
